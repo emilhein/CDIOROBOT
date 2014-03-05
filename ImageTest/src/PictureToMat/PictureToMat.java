0bba105c3d2fe70514033d7cb458e1bcf22d02b8
Emil - ændringer af style.xss og index.jsp <Emil@10.16.106.85>
2014-03-05 10:44:03 +0100

    Signed-off-by: Emil - ændringer af style.xss og index.jsp <Emil@10.16.106.85>

---
+++ ImageTest/src/PictureToMat/PictureToMat.java
@@ -0,0 +1,58 @@
+package PictureToMat;
+
+//import java.awt.image.BufferedImage;
+//import java.io.ByteArrayInputStream;
+//import java.io.IOException;
+//import java.io.InputStream;
+
+//import javax.imageio.ImageIO;
+
+import org.opencv.core.*;//Mat
+//import org.opencv.core.MatOfRect;
+//import org.opencv.core.Point;
+//import org.opencv.core.Rect;
+//import org.opencv.core.Scalar;
+import org.opencv.highgui.Highgui;
+
+//import org.opencv.objdetect.CascadeClassifier;
+
+public class PictureToMat {
+
+	public static void main(String[] args) {
+		// int[] test = new int[10];
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		System.out.println("\nRunning FaceDetector");
+
+		// Mat image = Highgui.imread("test.JPG"); // ge BGR
+		Mat m = Highgui.imread("test2.bmp"); // ge BGR
+		System.out.println("The picture has a total of " + m.total()
+				+ " pixels");
+		// System.out.println(m.dump());
+		/*
+		 * int cols = m.cols(); int rows = m.rows(); int elemSize =
+		 * (int)m.elemSize(); byte[] data = new byte[cols * rows * elemSize];
+		 */
+		// double[] rgb = m.get(0, 0);
+
+		// System.out.println(m.dump());
+
+		for (int j = 0; j < m.cols(); j++) {
+			for (int b = 0; b < m.rows(); b++) {
+				double[] rgb = m.get(j, b);
+				for (int i = 0; i < rgb.length; i = i + 3) {
+					System.out.println("Pixelnr " + "("+j+","+b+") " + "red:" + rgb[i]
+							+ " green:" + rgb[i + 1] + " blue:" + rgb[i + 2]);
+					if(rgb[i]==255){
+						m.put(b, j, 10.0);
+						System.out.println("hej");
+					}
+				}
+			}
+		}
+
+		
+		// System.out.println(image.dump());
+
+	}
+
+}
---
+++ ImageTest/src/TakePicture.java
@@ -0,0 +1,61 @@
+import org.opencv.core.*;
+import org.opencv.highgui.Highgui;        
+import org.opencv.highgui.VideoCapture;        
+        
+public class TakePicture {
+    public static void main (String args[]){
+    	System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+    	VideoCapture camera = new VideoCapture(0);
+    	
+    	if(!camera.isOpened()){
+    		System.out.println("Error");
+    	}
+    	else {
+    		Mat frame = new Mat();
+    	    while(true){
+    	    	if (camera.read(frame)){
+    	    		System.out.println("Frame Obtained");
+    	    		System.out.println("Captured Frame Width " + 
+    	    		frame.width() + " Height " + frame.height());
+    	    		
+    	    		Highgui.imwrite("camera.bmp", frame);
+    	    		System.out.println("OK");
+    	    		
+    	    		
+    	    		System.out.println();
+    	    		
+    	    		break;
+    	    	}
+    	    }	
+    	}
+    	camera.release();
+    }
+}   
+/*
+
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.Rect;
+
+public class Hello
+{
+   public static void main( String[] args )
+   {
+      System.loadLibrary( Core.NATIVE_LIBRARY_NAME );
+     
+      Mat mat1 = Mat.zeros( 10, 10, CvType.CV_8UC1 );
+      System.out.println();
+      System.out.println();
+      System.out.println( "mat1 := ");
+      System.out.println( " " + mat1.dump() );
+      
+      
+      System.out.println();
+      System.out.println();
+      System.out.println( "mat1 := ");
+      System.out.println( " " + mat1.dump() );
+      
+   }
+}
+*/
