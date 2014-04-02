839ec5d3f3c5e68cb1c0c7016f4f698e45a5decc
Oliver <Oliver@Oliver>
2014-04-02 11:17:57 +0200

    og igen

---
+++ ImageTest/src/dist/Punkt.java
@@ -0,0 +1,15 @@
+package dist;
+
+public class Punkt {
+    double x, y;
+    Punkt (double xkoord, double ykoord) {
+        this.x = xkoord;
+        this.y = ykoord;
+    }
+    public double getX() {
+        return x;
+    }
+    public double getY() {
+        return y;
+    }
+}
