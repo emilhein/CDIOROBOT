8ee5861591c0a921cb422c809185a62e3063142b
Sædvanlige ændringer :D <Emil@10.16.106.85>
2014-04-08 17:36:41 +0200

    Nu kan der leges med rutealgoritmen, for at finde hurtigste vej
    Signed-off-by: Sædvanlige ændringer :D <Emil@10.16.106.85>

--- ImageTest/src/Coordinates/FictiveCoordinates.java
+++ ImageTest/src/Coordinates/FictiveCoordinates.java
@@ -18,22 +18,22 @@ public class FictiveCoordinates {
 		//maks 470 (y) - de ulige
 
 		Coordi[0] = 10;	//x
-		Coordi[1] = 500;	//y
-		Coordi[2] = 470;   	//x
+		Coordi[1] = 170;	//y
+		Coordi[2] = 430;   	//x
 		Coordi[3] = 10;	//y
 		Coordi[4] = 355;	//x
 		Coordi[5] = 211;	//y
-		Coordi[6] = 598;	//x
+		Coordi[6] = 198;	//x
 		Coordi[7] = 398;	//y
 		Coordi[8] = 222;	//x
 		Coordi[9] = 222;	//y
 		Coordi[10] = 544;	//x
-		Coordi[11] = 469;	//y
+		Coordi[11] = 369;	//y
 		Coordi[12] = 123;	//x
 		Coordi[13] = 321;	//y
-		Coordi[14] = 600;	//x
+		Coordi[14] = 220;	//x
 		Coordi[15] = 400;	//y
-		Coordi[16] = 555;	//x
+		Coordi[16] = 455;	//x
 		Coordi[17] = 444;	//y
 		Coordi[18] = 332;	//x
 		Coordi[19] = 223;	//y
--- ImageTest/src/routeTest/RouteTest.java
+++ ImageTest/src/routeTest/RouteTest.java
@@ -1,13 +1,20 @@
 package routeTest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import Coordinates.FictiveCoordinates;
 
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
 import org.opencv.highgui.Highgui;
-
+ 
 public class RouteTest {
-
+	static List<Integer> xCoor = new ArrayList<>();
+	static List<Integer> yCoor = new ArrayList<>();
+	
 	public static void main(String[] args) {
 
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
@@ -26,7 +33,6 @@ public class RouteTest {
 		
 		
 		
-		
 		for(int c=0;c<Coordi.length; c=c+2){
 		//tegner firkant på koordinatplads i sort
 		for (int i = 0; i < 10; i++) {
@@ -35,11 +41,21 @@ public class RouteTest {
 				
 			}
 		}
-		System.out.println("Punkt nr: " + c);
+		xCoor.add(Coordi[c]);
+		yCoor.add(Coordi[c+1]);
 		}
+		for(int i=0;i<xCoor.size()-1;i++){
+			Core.line(frame, new Point(xCoor.get(i)+5,yCoor.get(i)+5), new Point(xCoor.get(i+1)+5, yCoor.get(i+1)+5), new Scalar(i*200,i*20,i*30), 2);	
+			
+		}
+		
 		Highgui.imwrite("RouteTest.jpg", frame);
 		System.out.println("DONE");
-		System.out.println(Coordi.length);
-
+	
+		
+		System.out.println("Koordinater til første prik er ("+xCoor.get(0)+","+yCoor.get(0)+")");
+		System.out.println("Koordinater til anden prik er ("+xCoor.get(1)+","+yCoor.get(1)+")");
+		System.out.println("Koordinater til tredje prik er ("+xCoor.get(2)+","+yCoor.get(2)+")");
+	
 	}
 }
