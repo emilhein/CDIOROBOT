ed118140a2b99de058b155d0a6abb04cf5d5ebfd
Henrik Krüger-Jensen <henrikkrugerjensen@gmail.com>
2014-05-26 13:03:04 +0200

    gui

---
+++ ImageTest/src/CallibratorGUI/CallibratorGUI.java
@@ -0,0 +1,107 @@
+package CallibratorGUI;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class CallibratorGUI  {
+
+
+	static JFrame frame1;
+	static Container pane;
+	static JButton btnApply;
+	static JLabel lblDP, lblCirkleDIst, lblParameter1, lblParameter2, lblMinradius, lblMaxradius;
+	static JTextField txtDP, txtCirkleDIst, txtParameter1, txtParameter2, txtMinradius, txtMaxradius;
+	static Insets insets;
+
+
+
+	public static void main (String args[]){
+
+		//Opretter rammen
+
+		frame1 = new JFrame ("CallibratorGUI");
+
+		//Sætter størrelsen af rammen i pixelx 
+		frame1.setSize (1200,500);
+
+		//Prepare panel
+		pane = frame1.getContentPane();
+
+		insets = pane.getInsets();
+
+		//tilføj layout for null
+		pane.setLayout (null);
+
+
+
+		btnApply = new JButton ("Apply");
+		lblDP = new JLabel ("DP:");
+		lblCirkleDIst = new JLabel ("Cirkle Dist:");
+		lblParameter1 = new JLabel ("Parameter 1:");
+		lblParameter2 = new JLabel ("Parameter 2:");
+		lblMinradius = new JLabel ("Min radius:");
+		lblMaxradius = new JLabel ("Max radius:");
+		txtDP = new JTextField (10);
+		txtCirkleDIst = new JTextField  (10);
+		txtParameter1 = new JTextField  (10);
+		txtParameter2 = new JTextField  (5);
+		txtMinradius = new JTextField  (10);
+		txtMaxradius = new JTextField  (10);
+
+		pane.add (lblDP); //tilføjer lblDP
+		lblDP.setBounds (insets.left + 5, insets.top + 5, lblDP.getPreferredSize().width, lblDP.getPreferredSize().height);
+
+
+		//Tilføjer alle komponenter
+		pane.add (lblDP);
+		pane.add (lblCirkleDIst);
+		pane.add (lblParameter1);
+		pane.add (lblParameter2);
+		pane.add (lblMinradius);
+		pane.add (lblMaxradius);
+		pane.add (txtDP);
+		pane.add (txtCirkleDIst);
+		pane.add (txtParameter1);
+		pane.add (txtParameter2);
+		pane.add (txtMinradius);
+		pane.add (txtMaxradius);
+		pane.add (btnApply);
+
+
+		//		//Placerer alle kompoenter
+		lblDP.setBounds (lblDP.getX() + lblDP.getWidth() + 5, insets.top + 5, lblDP.getPreferredSize().width, lblDP.getPreferredSize().height);
+		txtDP.setBounds (txtDP.getX() + txtDP.getWidth() + 5, insets.top + 20, txtDP.getPreferredSize().width, txtDP.getPreferredSize().height);
+
+		lblCirkleDIst.setBounds (lblCirkleDIst.getX() + lblCirkleDIst.getWidth() + 5, insets.top + 50, lblCirkleDIst.getPreferredSize().width, lblCirkleDIst.getPreferredSize().height);
+		txtCirkleDIst.setBounds (txtCirkleDIst.getX() + txtCirkleDIst.getWidth() + 5, insets.top + 65, txtCirkleDIst.getPreferredSize().width, txtCirkleDIst.getPreferredSize().height);
+
+		lblParameter1.setBounds (lblParameter1.getX() + lblParameter1.getWidth() + 5, insets.top + 95, lblParameter1.getPreferredSize().width, lblParameter1.getPreferredSize().height);
+		txtParameter1.setBounds (txtParameter1.getX() + txtParameter1.getWidth() + 5, insets.top + 110, txtParameter1.getPreferredSize().width, txtParameter1.getPreferredSize().height);
+
+		lblParameter2.setBounds (lblParameter2.getX() + lblParameter2.getWidth() + 5, insets.top + 140, lblParameter2.getPreferredSize().width, lblParameter2.getPreferredSize().height);
+		txtParameter2.setBounds (txtParameter2.getX() + txtParameter2.getWidth() + 5, insets.top + 155, txtParameter2.getPreferredSize().width, txtParameter2.getPreferredSize().height);
+
+		lblMinradius.setBounds (lblMinradius.getX() + lblMinradius.getWidth() + 5, insets.top + 185, lblMinradius.getPreferredSize().width, lblMinradius.getPreferredSize().height);
+		txtMinradius.setBounds (txtMinradius.getX() + txtMinradius.getWidth() + 5, insets.top + 200, txtMinradius.getPreferredSize().width, txtMinradius.getPreferredSize().height);
+
+
+		lblMaxradius.setBounds (lblMaxradius.getX() + lblMaxradius.getWidth() + 5, insets.top + 230, lblMaxradius.getPreferredSize().width, lblMaxradius.getPreferredSize().height);
+		txtMaxradius.setBounds (txtMaxradius.getX() + txtMaxradius.getWidth() + 5, insets.top + 245, txtMaxradius.getPreferredSize().width, txtMaxradius.getPreferredSize().height);
+
+
+		btnApply.setBounds (btnApply.getX() + btnApply.getWidth() + 5, insets.top + 275, btnApply.getPreferredSize().width, btnApply.getPreferredSize().height);
+
+
+		//Gør rammen synlig
+		frame1.setVisible (true);
+
+		try {UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());}	
+		catch (ClassNotFoundException e) {}
+		catch (InstantiationException e) {}
+		catch (IllegalAccessException e) {}
+		catch (UnsupportedLookAndFeelException e) {}		
+
+	}
+
+
+}
