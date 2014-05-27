4807076784b9e5c7fee97b2e1507353e20dc13be
Henrik Krüger-Jensen <henrikkrugerjensen@gmail.com>
2014-05-27 15:56:50 +0200

    change

--- ImageTest/src/main/Main.java
+++ ImageTest/src/main/Main.java
@@ -14,6 +14,7 @@ import lejos.pc.comm.NXTConnector;
 import lejos.pc.comm.NXTInfo;
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Main {
 
@@ -25,31 +26,26 @@ public class Main {
 		ballMethod balls = new ballMethod();
 
 		/*
-		*Standardværdier for disse argumenter plejer at være 4,8,19 eller 30,40,2
-		*/
-		float[] RoboCoor = balls.findCircle(6,12,2);//minradius, maxrdius, antalbolde
+		 *Standardværdier for disse argumenter plejer at være 4,8,19 eller 30,40,2
+		 */
+		float[] RoboCoor = balls.findCircle(6,13,2);//minradius, maxrdius, antalbolde
 
 		Mat frame = Highgui.imread("AfterColorConvert.jpg"); // henter det konverterede billlede
 		for(int i = 0; i < RoboCoor.length;i=i+3){
 			System.out.println("Bold nr " + i +" ligger på "+Math.round(RoboCoor[i]) + ","+Math.round(RoboCoor[i+1]));
-			
 		}
-		
-		
+
 		double[] front = frame.get(Math.round(RoboCoor[1]), Math.round(RoboCoor[0])); ///X OG Y ER FUCKED
 		//double red = front[2]; //henter en rød farver fra den ene cirkel
 		double blue = front[0];
 		double green = front[1];
 		double red = front[2];
-		
-		
+
 		System.out.println("Har farverne = "+(int)blue + (int)red+(int)green);
-		
-		
+
 		double[] back = frame.get(Math.round(RoboCoor[4]), Math.round(RoboCoor[3])); /// X OG Y ER FUCKED
 		double red2 = back[2]; // henter en rød farve ([2]) fra den anden cirkel
-	
-		
+
 		Punkt roboFrontPunkt = new Punkt(0,0);
 		Punkt roboBagPunkt = new Punkt(0,0);
 		// herunder sættes robotpunket, alt efter hvilken cirkel der er rød.
@@ -69,27 +65,39 @@ public class Main {
 		/* 
 		System.out.println("Dette er rød1 farven = " + red);
 		System.out.println("Dette er rød2 farven = " + red2);
-		*/
-		
-		float[] ballCoor = balls.findCircle(4, 8, 5); // finder bolde 6,12,6
+		 */
+
+		float[] ballCoor = balls.findCircle(2, 6, 3); // finder bolde 6,12,6
 
 		RouteTest.drawBallMap(ballCoor, roboBagPunkt, roboFrontPunkt); // tegner dem i testprogrammet
 
 		minPunkt = RouteTest.drawBallMap(ballCoor, roboBagPunkt, roboFrontPunkt); // tegner dem i testprogrammet
+		int tempx=minPunkt.getY();
+		int tempy=minPunkt.getX();
+		minPunkt.setX(tempx);
+		minPunkt.setY(tempy);
 
-		System.out.println("koordinaterne til frontpunkt er (" + roboBagPunkt.getX() +","+roboBagPunkt.getY()+")");
-		System.out.println("koordinaterne til bagpunkt er (" + roboFrontPunkt.getX() +","+roboFrontPunkt.getY()+")");
- 
-/*
+		System.out.println("koordinaterne til Bagpunkt er (" + roboBagPunkt.getX() +","+roboBagPunkt.getY()+")");
+		System.out.println("koordinaterne til Frontpunkt er (" + roboFrontPunkt.getX() +","+roboFrontPunkt.getY()+")");
+		System.out.println("koordinaterne til MinPunkt er (" + minPunkt.getX() +","+minPunkt.getY()+")");
+
+		Punkt nyRoboFront = new Punkt(roboFrontPunkt.getX()-roboBagPunkt.getX(),roboFrontPunkt.getY()-roboBagPunkt.getY());
+		Punkt nyRoboBag = new Punkt(0,0);
+		Punkt nyMinPunkt = new Punkt(minPunkt.getX()-roboBagPunkt.getX(),minPunkt.getY()-roboBagPunkt.getY());
+		System.out.println("koordinaterne til nyBagpunkt er (" + nyRoboBag.getX() +","+nyRoboBag.getY()+")");
+		System.out.println("koordinaterne til nyFrontpunkt er (" + nyRoboFront.getX() +","+nyRoboFront.getY()+")");
+		System.out.println("koordinaterne til nyMinpunkt er (" + nyMinPunkt.getX() +","+nyMinPunkt.getY()+")");
 		
 		CalcAngle Angle = new CalcAngle();
-		int BallAngle = Angle.Calcangle(roboBagPunkt, minPunkt);
-		int RoboAngle = Angle.Calcangle(roboBagPunkt, roboFrontPunkt);
+		int BallAngle = Angle.Calcangle(nyRoboBag, nyMinPunkt);
+		System.out.println("BallAngle = " + BallAngle);
+		int RoboAngle = Angle.Calcangle(nyRoboBag, nyRoboFront);
+		System.out.println("RoboAngle = " + RoboAngle);
 		int TurnAngle = BallAngle - RoboAngle;
-		
+
 		CalcDist dist = new CalcDist();
-		int minLength = dist.Calcdist(roboFrontPunkt, minPunkt);
-		
+		int minLength = Math.abs(dist.Calcdist(roboFrontPunkt, minPunkt));
+
 		try{ 
 			//prøver at forbinde til vores robot
 			NXTInfo nxtInfo = new NXTInfo(2,"G9 awesome!","0016530918D4");
@@ -100,44 +108,59 @@ public class Main {
 			//åbner streams
 			OutputStream dos = connt.getOutputStream();
 			InputStream dis = connt.getInputStream();
-			
-			int Case;
-			int i;
-			int angle = TurnAngle/5;	//vinkel konvertering
-			System.out.println("angle " + angle);
-			if(angle < 0) 				//vælger retning der skal drejes
-				Case = 11;				
-			else Case = 22;
 
-			dos.write(Case);			//sender case
-			dos.flush();
-			dos.write(angle);			//sender vinkel
-			dos.flush();
+			Scanner scan = new Scanner(System.in);
+			while(true){
+				System.out.println("Waiting for your go!");	
+				int input = scan.nextInt();
 
-			//venter på at motorerne ikke kører længere
-			int u = dis.read();			
-			while(u==1){
-				u = dis.read();
+				int Case;
+				int i;
+				System.out.println("TurnAngle = " + TurnAngle);
+				int angle = TurnAngle*2;	//vinkel konvertering
+				System.out.println("angle " + angle);
+				if(angle > 0) 				//vælger retning der skal drejes
+					Case = 11;				
+				else Case = 22;
+				angle = Math.abs(angle);
+				dos.write(Case);			//sender case
+				dos.flush();
+				dos.write(angle);			//sender vinkel
+				dos.flush();
+
+				//				//venter på at motorerne ikke kører længere
+				//				int u = dis.read();			
+				//				while(u==1){
+				//					u = dis.read();
+				//				}
+
+				Thread.sleep(2000);
+				//kører robot frem
+				int distance = (minLength );	//længde konvertering
+				System.out.println("dist = " + distance);
+				dos.write(81);
+				dos.flush();
+				i = distance;
+				dos.write(i);
+				dos.flush();
+
+				//				//venter på at motorerne ikke kører længere
+				//				int j = dis.read();			
+				//				while(j==1){
+				//					j = dis.read();
+				//				}
+
+				Thread.sleep(2000);
+
+				//samler bold op
+				dos.write(51);				
+				dos.flush();
+				dos.write(51);
+				dos.flush();	
+				Thread.sleep(2000);
 			}
-
-			//kører robot frem
-			int distance = (minLength * 3) - 150;	//længde konvertering
-			System.out.println("dist = " + distance);
-			dos.write(61);
-			dos.flush();
-			i = distance;
-			dos.write(i);
-			dos.flush();
-
-			//samler bold op
-			dos.write(31);				
-			dos.flush();
-			dos.write(31);
-			dos.flush();			
-
 		}
-		catch(Exception ex){System.out.println(ex);}*/
+		catch(Exception ex){System.out.println(ex);}
 	}
+
 }
-
-
