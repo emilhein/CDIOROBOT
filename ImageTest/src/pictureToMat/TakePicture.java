f6050ab2715a28cad097b6bf22c450b38fbc71c3
Oliver <Oliver@10.16.129.237>
2014-05-30 14:02:07 +0200

    Signed-off-by: Oliver <Oliver@10.16.129.237>

--- ImageTest/src/pictureToMat/TakePicture.java
+++ ImageTest/src/pictureToMat/TakePicture.java
@@ -10,7 +10,7 @@ public class TakePicture {
 //XXX
 	public  void takePicture() {
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME); 
-    	VideoCapture camera = new VideoCapture(0);
+    	VideoCapture camera = new VideoCapture(1);
     	for (int i = 0; i<1;i++){//flere billeder
     	if(!camera.isOpened()){
     		System.out.println("Error");
@@ -22,7 +22,6 @@ public class TakePicture {
     	    		System.out.println("Frame Obtained");
     	    		System.out.println("Captured Frame Width " + 
     	    		frame.width() + " Height " + frame.height());
-    	    		
     	    		//	Highgui.imwrite("C:/Users/Emil/Desktop/Pictures/Billed"+i+".jpg", frame);
     	    			Highgui.imwrite("billed0.png", frame);
 
