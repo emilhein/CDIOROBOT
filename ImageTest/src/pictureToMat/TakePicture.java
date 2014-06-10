7edfba201be8ed4c7396d29e92027fa7125c79db
Martineee <bengtssonmartine@gmail.com>
2014-06-10 08:40:35 +0200

    l

--- ImageTest/src/pictureToMat/TakePicture.java
+++ ImageTest/src/pictureToMat/TakePicture.java
@@ -5,15 +5,14 @@ import org.opencv.highgui.Highgui;
 import org.opencv.highgui.VideoCapture;        
         
 public class TakePicture {
-    public  void main (String args[]){
-    	
-    }
-//XXX
-	public  void takePicture() {
-		System.loadLibrary(Core.NATIVE_LIBRARY_NAME); 
+
+	public  void takePicture()
+	{
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		
     	VideoCapture camera = new VideoCapture(0);// us 1 if two cameras a connected (including integrated webcam)
-    /*	boolean wset =*/camera.set(Highgui.CV_CAP_PROP_FRAME_WIDTH, 1600);
-    /*	boolean hset = */camera.set(Highgui.CV_CAP_PROP_FRAME_HEIGHT, 900);
+    	camera.set(Highgui.CV_CAP_PROP_FRAME_WIDTH, 1600); /* width of camera image */
+    	camera.set(Highgui.CV_CAP_PROP_FRAME_HEIGHT, 900); /* height of camera image */
 	
     	if(!camera.isOpened()){
     		System.out.println("Error");
@@ -23,28 +22,21 @@ public class TakePicture {
     		Mat frame = new Mat();
     	    while(true){
     	    	
-    	    	if (camera.read(frame)){
+    	    	if (camera.read(frame))
+    	    	{
     	    		System.out.println("Frame Obtained");
     	    		System.out.println("Captured Frame Width " + 
     	    		frame.width() + " Height " + frame.height());
     	    		
-    	    		//	Highgui.imwrite("C:/Users/Emil/Desktop/Pictures/Billed"+i+".jpg", frame);
-    	    			Highgui.imwrite("billed0.png", frame);
-    	  
-
-
-
-    	    		}
-    	    		System.out.println("Picture saved");
-    	    		
-    	    		
-    	    		System.out.println();
-    	    		
-    	    		break;
+    	    		Highgui.imwrite("billed0.png", frame);
     	    	}
-    	    }	
+    	    		
+    	    	System.out.println("Picture saved");
+    	    	System.out.println();
+    	    	break;
+    	    }
+    	}	
     	
     	camera.release();
 	}
-}   
-
+}
