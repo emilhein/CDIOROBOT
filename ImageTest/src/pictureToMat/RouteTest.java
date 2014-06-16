b2ff6206bad10a4cc4680508887cf0313266aab4
Sædvanlige ændringer :D <Emil@10.16.106.85>
2014-06-16 13:18:33 +0200

    Signed-off-by: Sædvanlige ændringer :D <Emil@10.16.106.85>

--- ImageTest/src/pictureToMat/RouteTest.java
+++ ImageTest/src/pictureToMat/RouteTest.java
@@ -79,6 +79,7 @@ public class RouteTest {
 		Core.line(frame, new Point(roboBagPunkt.x(), roboBagPunkt.y()),	new Point(minPunkt.x() +10, minPunkt.y() + 10),	new Scalar(200, 120, 45), 4);
 		
 		
+		Core.rectangle(frame, new Point(100,100), new Point(300,300), null, 1);
 		paintPoint(frame,new CvPoint(1600/2, 900/2), 0, 128, 128,30); // midten af billedet
 		paintPoint(frame,new CvPoint((goalA.x()-((int)(90*ppcm))), goalA.y()), 39, 127, 255,20); // midten af banen
 
