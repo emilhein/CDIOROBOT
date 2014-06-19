24bfb571dbc75440094fcba59a6178de165e684d
Oliver <Oliver@Oliver>
2014-06-19 12:04:55 +0200

    Signed-off-by: Oliver <Oliver@Oliver>

--- ImageTest/src/main/MainTest.java
+++ ImageTest/src/main/MainTest.java
@@ -1,21 +1,16 @@
 package main;
 import pictureToMat.DetectRects2;
+import pictureToMat.TakePicture;
 
 public class MainTest {
 	
 	public static void main(String args[])
 	{
-		DetectRects2 DT = new DetectRects2();
+		TakePicture tk = new TakePicture();
 
-		for(int i = 0; i < 100; i++)
+		for(int i = 0; i < 10; i++)
 		{
-			System.out.println(i);
-			DT.detectAllRects();;
-			System.out.println(DT.getPixPerCm());
-			System.out.println(DT.getGoalA());
-			System.out.println(DT.getGoalB());
-			System.out.println(DT.getInnerRect());
-			System.out.println(DT.getObstruction());
+			tk.takePicture();
 		}
 	}
 }
