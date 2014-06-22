e1f743cf068fb0e26bb4e78564698d9b42101a15
Henrik Krüger-Jensen <henrikkrugerjensen@gmail.com>
2014-06-22 11:27:50 +0200

    Signed-off-by: Henrik Krüger-Jensen <henrikkrugerjensen@gmail.com>

--- ImageTest/src/Controller/PrimaryController.java
+++ ImageTest/src/Controller/PrimaryController.java
@@ -491,6 +491,5 @@ public class PrimaryController {
 		roboBagPunkt.y(tempPoint.y()+diffY);
 		System.out.println("front " + roboFrontPunkt.x() + " " + roboFrontPunkt.y());
 		System.out.println("bag " + roboBagPunkt.x() + " " + roboBagPunkt.y());
-
 	}
 }
